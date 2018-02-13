3e11f3c7afbd6d17d15b067bac1aced0bf7f0ff6
Harmen Stoppels <harmenstoppels@gmail.com>
2018-02-13 09:11:01 +0200

    WIP symbolic basis funcs

--- src/symbolic_basis_functions.jl
+++ src/symbolic_basis_functions.jl
@@ -1,8 +1,75 @@
-function basis_coefficients(p::Expr, x::NTuple{nbasis,NTuple{dim,T}}, vars::NTuple{dim,Symbol}) where {nbasis,dim,T}
-    A = zeros(nbasis, nbasis)
+"""
+    simplify(:(0.0 * f(3x) + 1.0 * 3.0)) -> 3.0
+
+Simplify an expression
+"""
+simplify(e::Expr) = _simplify(e)
+
+_simplify(e) = e
+
+function _simplify(e::Expr)
+    if e.head == :call
+        op = e.args[1]
+        args = [_simplify(arg) for arg in e.args[2:end]]
+        
+        if op == :*
+            # Fold numbers
+            prod = mapreduce(x -> isa(x, Number) ? x : 1, *, 1, args)
+            filter!(x -> !isa(x, Number), args)
+
+            # Simplify
+            if prod == 0
+                return 0
+            end
+
+            if length(args) == 0
+                return prod
+            end
+
+            if abs(prod) == 1
+                if length(args) == 1
+                    if prod == 1
+                        return args[1]
+                    else
+                        return Expr(:call, :-, args[1])
+                    end
+                else
+                    return Expr(:call, :*, args...)
+                end
+            end
+
+            return Expr(:call, :*, prod, args...)
+        elseif op == :+
+            sum = mapreduce(x -> isa(x, Number) ? x : 0, +, 0, args)
+            filter!(x -> !isa(x, Number), args)
+
+            if length(args) == 0
+                return sum
+            end
+
+            if sum == 0
+                if length(args) == 1
+                    return args[1]
+                else
+                    return Expr(:call, :+, args...)
+                end
+            end
+
+            return Expr(:call, :+, sum, args...)
+        else
+            return Expr(:call, op, args...)
+        end
+    end
+
+    return e
+end
+
+function basis_coefficients(p::Expr, x::NTuple{n,NTuple{d,T}}, vars::NTuple{d,Symbol}) where {n,d,T}
+    A = zeros(n, n)
+
     sandbox = Module()
 
-    for i = 1:nbasis, j = 1:nbasis
+    for i = 1:n, j = 1:n
 
         # Fill in the variables
         for (variable, value) = zip(vars, x[j])
@@ -17,53 +84,53 @@ function basis_coefficients(p::Expr, x::NTuple{nbasis,NTuple{dim,T}}, vars::NTup
 end
 
 """
-    simplify(:(0.0 * f(3x) + 1.0 * 3.0)) -> 3.0
-
-Simplify an expression
+Construct the basis functions in the nodes
 """
-function simplify(e::Expr)
-    _simplify(e)
-end
-
-_simplify(e) = e
-
-function _simplify(e::Expr)
-    if e.head == :call
-        op = e.args[1]
-        args = [ _simplify(arg) for arg in e.args[2:end] ]
-        
-        if op == :*
-            0 in args && return 0
-            filter!(x -> x != 1, args)
-            length(args) == 1 && return args[1]
-            length(args) == 0 && return 1
-        elseif op == :+
-            filter!(x -> x != 0, args)
-            length(args) == 1 && return args[1]
-            length(args) == 0 && return 0
-        end
-
-        return Expr(:call, op, args...)
-    end
-
-    return e
-end
-
 function polynomials(p::Expr, A::AbstractMatrix)
     Ainv = inv(A)
-    nbasis = size(A, 1)
+    n = size(A, 1)
     basis_functions = []
 
-    for j = 1 : nbasis
-        args = [:($(Ainv[i,j]) * $(p.args[i + 1])) for i = 1 : nbasis]
+    for j = 1 : n
+        args = [:($(Ainv[i,j]) * $(p.args[i + 1])) for i = 1 : n]
         push!(basis_functions, simplify(Expr(:call, :+, args...)))
     end
 
     return basis_functions
 end
 
-function create_basis(p::Expr, coords::NTuple{nbasis,NTuple{dim,T}}, vars::NTuple{dim,Symbol}) where {nbasis,dim,T}
-    return polynomials(p, basis_coefficients(p, coords, vars))
+
+function create_basis(p::Expr, nodes::NTuple{n,NTuple{d,T}}, vars::NTuple{d,Symbol}) where {n,d,T}
+    return polynomials(p, basis_coefficients(p, nodes, vars))
 end
 
 create_basis_gradients(ϕs, vars) = [[differentiate(ϕ, x) for x in vars] for ϕ in ϕs]
+
+"""
+Evaluate the basis in the given node
+"""
+function evalute_basis(ϕ::Expr, ∇ϕ::Vector{Expr}, vars::NTuple{d,Symbol}) where {d}
+    unpacking = Expr(:block)
+
+    for (idx, var) in enumerate(vars)
+        push!(unpacking.args, :($var = node[$idx]))
+    end
+
+    ∇ϕ_eval = :(SVector())
+
+    for ∂ϕ in ∇ϕ
+        push!(∇ϕ_eval.args, ∂ϕ)
+    end
+
+    return quote
+        function testtest(node::NTuple{n,T}) where {n,T}
+            $unpacking
+            $ϕ, $∇ϕ_eval
+        end
+    end
+end
+
+struct ReferenceBasis{d}
+    ϕ::Float64
+    ∇ϕ::SVector{d,Float64}
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,43 +1,16 @@
 using Base.Test
 using FEM
 
-function test_basis_functions_on_nodes(ϕs, ns, vars)
+function test_basis_functions_on_nodes(ϕs, nodes, vars)
     sandbox = Module()
 
     # Test whether ϕᵢ(nⱼ) = δᵢⱼ
     for i = 1 : 3, j = 1 : 3
-        for (var, val) in zip(vars, ns[j])
+        for (var, val) in zip(vars, nodes[j])
             eval(sandbox, :($var = $val))
         end
         value = eval(sandbox, ϕs[i])
         @test i == j ? value == 1 : value == 0
-    end
-end
-
-@testset "Creating basis functions" begin
-    @testset "2D example" begin
-        ns = ((0.0, 0.0), (1.0, 0.0), (0.0, 1.0))
-        vars = (:u, :v)
-        ϕs = FEM.create_basis(:(1 + u + v), ns, vars)
-
-        @test ϕs[1] == :(1 + -1.0u + -1.0v)
-        @test ϕs[2] == :u
-        @test ϕs[3] == :v
-
-        test_basis_functions_on_nodes(ϕs, ns, vars)
-    end
-
-    @testset "3D example" begin
-        ns = ((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0))
-        vars = (:u, :v, :w)
-        ϕs = FEM.create_basis(:(1 + u + v + w), ns, vars)
-
-        @test ϕs[1] == :(1 + -1.0u + -1.0v + -1.0w)
-        @test ϕs[2] == :u
-        @test ϕs[3] == :v
-        @test ϕs[4] == :w
-
-        test_basis_functions_on_nodes(ϕs, ns, vars)
     end
 end
 
@@ -49,4 +22,33 @@ end
     @test FEM.simplify(:(0 + 0 + 0)) == :(0)
     @test FEM.simplify(:(f(0 * x + 0))) == :(f(0))
     @test FEM.simplify(:(0 * f(3))) == :(0)
+    @test FEM.simplify(:(-1 * x)) == :(-x)
+end
+
+@testset "Creating basis functions" begin
+    @testset "2D example" begin
+        nodes = ((0., 0.), (1., 0.), (0., 1.))
+        vars = (:u, :v)
+        ϕs = FEM.create_basis(:(1 + u + v), nodes, vars)
+
+        @test ϕs[1] == :(1 + -u + -v)
+        @test ϕs[2] == :u
+        @test ϕs[3] == :v
+
+        test_basis_functions_on_nodes(ϕs, nodes, vars)
+    end
+
+    @testset "3D example" begin
+        nodes = ((0., 0., 0.), (1., 0., 0.), (0., 1., 0.), (0., 0., 1.))
+        vars = (:u, :v, :w)
+        ϕs = FEM.create_basis(:(1 + u + v + w), nodes, vars)
+        test_basis_functions_on_nodes(ϕs, nodes, vars)
+    end
+
+    @testset "2D hp-FEM" begin
+        nodes = ((0., 0.), (1., 0.), (0., 1.), (0.5, 0.5))
+        vars = (:u, :v)
+        ϕs = FEM.create_basis(:(1 + u + v + u*v), nodes, vars)
+        test_basis_functions_on_nodes(ϕs, nodes, vars)
+    end
 end
